260349e0f9363256e851743963bd6cacce226f48
YugDhaigude <yugamol.dhaigude77@nmims.in>
2026-07-26 18:51:12 +0530

    feat: add BreadcrumbNav component to menu page

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -9,6 +9,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Badge } from '@/components/ui/badge'
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { BreadcrumbNav } from '@/components/BreadcrumbNav'
 
 export interface MenuItem {
   id: string
