2f64843743880ddea9598c86235e657cffa4d3ab
YugDhaigude <yugamol.dhaigude77@nmims.in>
2026-07-27 15:26:55 +0530

    Error fixes

---
+++ lib/createStore.ts
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react'
+
+export function create<T>(
+  createState: (
+    set: (partial: Partial<T> | ((state: T) => Partial<T> | T)) => void,
+    get: () => T
+  ) => T
+) {
+  let state: T
+  const listeners = new Set<() => void>()
+
+  const setState = (partial: Partial<T> | ((state: T) => Partial<T> | T)) => {
+    const nextState = typeof partial === 'function' ? (partial as any)(state) : partial
+    state = { ...state, ...nextState }
+    listeners.forEach((listener) => listener())
+  }
+
+  const getState = () => state
+
+  state = createState(setState, getState)
+
+  const useStore = () => {
+    const [, setTick] = useState(0)
+    useEffect(() => {
+      const listener = () => setTick((t) => t + 1)
+      listeners.add(listener)
+      return () => {
+        listeners.delete(listener)
+      }
+    }, [])
+    return state
+  }
+
+  Object.assign(useStore, {
+    getState,
+    setState,
+    subscribe: (listener: () => void) => {
+      listeners.add(listener)
+      return () => listeners.delete(listener)
+    },
+  })
+
+  return useStore
+}
--- lib/managerStore.ts
+++ lib/managerStore.ts
@@ -1,4 +1,4 @@
-import { create } from 'zustand'
+import { create } from './createStore'
 
 export interface Branch {
   id: string
